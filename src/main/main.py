23914e7c1093519e3c7b58e4a007b18ce6fe9981
ofelipelucca <felipe.lucca.t.oliveira@gmail.com>
2025-01-19 12:16:02 -0300

    Agora a main.py inicia com as portas dos websockets

--- src/main/main.py
+++ src/main/main.py
@@ -1,23 +1,30 @@
 from src.mainloop.mainloop import MainLoop
 from src.logger.logger import Logger
-import asyncio  
+import asyncio
 import sys
 
 logger = Logger.configure_application_logger()
 error_logger = Logger.configure_error_logger()
 
-async def main(data_port, frames_port) -> None:
+async def main(data_port: int, frames_port: int) -> None:
+    """
+    Inicializa o LibrasController com os servidores WebSocket.
+
+    Args:
+        data_port (int): Porta para o servidor WebSocket de dados.
+        frames_port (int): Porta para o servidor WebSocket de frames.
+    """
     logger.info("Iniciando LibrasController...")
+    main_loop = MainLoop(data_port, frames_port)
 
     try:
-        main_loop = MainLoop(data_port, frames_port)
-        main_loop.start()
+        await main_loop.start()
     except Exception as e:
         error_message = f"LibrasController: {e}"
         logger.error(error_message)
         error_logger.error(error_message)
     finally:
-        main_loop.stop()
+        await main_loop.stop()
         logger.info("LibrasController encerrado.")
 
 if __name__ == "__main__":
